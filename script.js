351a1e403d6ab6ad4e2a2f43387e32cf7e52657a
Nash Lochmann <136750155+gitnash9@users.noreply.github.com>
2023-09-24 13:44:09 -0500

    start of project

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const toDoForm = document.querySelector("to-do-form");
+const userInput = document.querySelector("#add-to-do");
+const toDoList = document.querySelector("to-do-list");
+
+toDoForm.addEventListener("submit", addToDoItem);
+
+function addToDoItem() {
+    const toDoForm = userInput.value;
+
+    // create a new list element
+    // append the list element to the <ul>
+}
+
+// Bonus - add the list to local storage and be able to retrieve it from local storage 
