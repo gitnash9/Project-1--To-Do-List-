9ba2d685b39f63698ce5d32a5d97c35959ac1000
Nash Lochmann <136750155+gitnash9@users.noreply.github.com>
2023-09-24 21:03:13 -0500

    not finished, I just do not have time to work

--- Project File/script.js
+++ Project File/script.js
@@ -1,15 +1,60 @@
-
-const toDoForm = document.querySelector("#to-do-form");
-const userInput = document.querySelector("#add-to-do");
-const toDoList = document.querySelector("#to-do-list");
-
-toDoForm.addEventListener("submit", addToDoItem);
-
-function addToDoItem() {
-    const toDoForm = userInput.value;
-
-    // create a new list element
-    // append the list element to the <ul>
+const listForm = document.getElementsByTagName('li');
+for (i = 0; i < listForm.length; i++) {
+  const span = document.createElement('SPAN');
+  const txt = document.createTextNode('\u00D7');
+  span.className = 'close';
+  span.appendChild(txt);
+  listForm[i].appendChild(span);
 }
+
+const deleteItem = document.getElementsByClassName('close');
+for (i = 0; i < deleteItem.length; i++) {
+  deleteItem[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+const checkedItem = document.querySelector('ul');
+checkedItem.addEventListener('click', function(fisishedItem) {
+  if (fisishedItem.target.tagName === 'li') {
+    finishedItem.target.classList.toggle('checked');
+  }
+}, false);
+
+const userInput = document.querySelector('#add-to-do')
+const toDoForm = document.querySelector('#to-do-form')
+const toDoList = document.querySelector('#to-do-list')
+
+toDoForm.addEventListener('submit', addToDoItem)
+
+function addToDoItem(Item) {
+    Item.preventDefault();
+    const toDo = userInput.value;
+
+    const newToDo = document.createElement('li')
+    newToDo.className = 'to-do';
+    newToDo.innerText = toDo;
+    newToDo.target = '_blank';
+
+    toDoList.appendChild(newToDo)
+
+    toDoForm.reset()
+
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          var div = this.parentElement;
+          div.style.display = "none";
+        }
+      }
+}
+
+
 
 // Bonus - add the list to local storage and be able to retrieve it from local storage 
---
+++ notes.md
@@ -0,0 +1,29 @@
+Outline for the Project:
+
+[HTML]
+  Step 1 - Create outline
+  Step 2 - Create <ul> and <li> 
+  -----------------
+  Finished
+
+[CSS]
+  Step 1 - Do after JS and HTML
+  Step 2 - Create classes/ids for styling
+  Step 3 - Edit styles
+  -------------------
+
+[JS]
+  Step 1 - Establish Selectors
+  Step 2 - Establish Event Listeners
+  Step 3 - Declare Functions
+  Step 4 - Create List Function 
+      Step 4.1 - They should be able to add and delete them
+      Step 4.2 - They should be able to mark them as complete
+  Step 5 - Storage
+  ----------------------
+
+*/ -------------------------------------------------------------------------------------------- /*
+
+[Objective] Create a To-Do list application where users can add tasks, mark them as complete, and delete them.
+
+[Features] Add a new task. Mark a task as complete. Delete a task. (Bonus) Store tasks in local storage so they persist on page reload.
